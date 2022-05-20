5ddf26cced58c5f55e4d38c0dcb463285edd9de3
senhorbento <sbento.ti@gmail.com>
2022-05-19 21:34:37 -0300

    feat: adicionado lista com velocidades de comunicacao serial

--- src/network.h
+++ src/network.h
@@ -4,10 +4,17 @@
 #include <Arduino.h>
 
 typedef struct{
-  int remetente;
-  int destino;
-  int mensagemEnviada;
+    int remetente;
+    int destino;
+    int mensagemEnviada;
 } TMensagem;
+
+int velocidadeSerial[] = {
+    4800, 
+    9600, 
+    19200, 
+    57600
+};
 
 class Network{
     private:
@@ -15,52 +22,52 @@ class Network{
         int mensagemRecebida;
         TMensagem mensagem;
     public:
+        boolean ChecarEndereco(int end);
         void IniciarComunicacao();
         void SetEndereco(int v);
-        boolean ChecarEndereco(int end);
         void EscreverMensagem(int remetente, int destino, int mensagem);
         void EnviarMensagem();
         void ReceberMensagem(TMensagem msg);
 };
 
+boolean Network::ChecarEndereco(int end){
+    return end == endereco ? 1 : 0;
+}
+
 void Network::IniciarComunicacao(){
-  Serial.begin(9600);
-  Serial3.begin(4800);
+    Serial.begin(VelocidadeSerial[0]);
+    Serial3.begin(VelocidadeSerial[3]);
 }
 
 void Network::SetEndereco(int v){
     endereco = v;
 }
 
-boolean Network::ChecarEndereco(int end){
-  return end == endereco ? 1 : 0;
-}
-
 void Network::EscreverMensagem(int remetente, int destino, int msg){
-  mensagem.remetente = remetente;
-  mensagem.destino = destino;
-  mensagem.mensagemEnviada = msg;
+    mensagem.remetente = remetente;
+    mensagem.destino = destino;
+    mensagem.mensagemEnviada = msg;
 }
 
 void Network::EnviarMensagem(){
     if(mensagem.remetente > endereco){
-      Serial3.print(mensagem.remetente);
-      Serial3.print(mensagem.destino);
-      Serial3.print(mensagem.mensagemEnviada);
+        Serial3.print(mensagem.remetente);
+        Serial3.print(mensagem.destino);
+        Serial3.print(mensagem.mensagemEnviada);
     }
     else{
-      Serial.print(mensagem.remetente);
-      Serial.print(mensagem.destino);
-      Serial.print(mensagem.mensagemEnviada);
+        Serial.print(mensagem.remetente);
+        Serial.print(mensagem.destino);
+        Serial.print(mensagem.mensagemEnviada);
     }
 }
 
 void Network::ReceberMensagem(TMensagem msg){
-  EscreverMensagem(msg.remetente, msg.destino, msg.mensagemEnviada);
-  if(ChecarEndereco(mensagem.remetente))
-    mensagemRecebida = mensagem.mensagemEnviada;
-  else
-    EnviarMensagem();
+    EscreverMensagem(msg.remetente, msg.destino, msg.mensagemEnviada);
+    if(ChecarEndereco(mensagem.remetente))
+        mensagemRecebida = mensagem.mensagemEnviada;
+    else
+        EnviarMensagem();
 }
 
 #endif
